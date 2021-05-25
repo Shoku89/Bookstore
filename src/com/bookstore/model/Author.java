9fb5751078def8302792847c0d7069717e66c526
Shoku89 <isarperes89@gmail.com>
2021-05-25 12:44:54 -0300

    model

---
+++ src/com/bookstore/model/Author.java
@@ -0,0 +1,41 @@
+package com.bookstore.model;
+
+public class Author {
+
+    private Integer id;
+    private String fullName;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/com/bookstore/model/Book.java
@@ -0,0 +1,41 @@
+package com.bookstore.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+
+    private Integer id;
+    private String title;
+    private Author author;
+    private List<Category> categories = new ArrayList<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+}
